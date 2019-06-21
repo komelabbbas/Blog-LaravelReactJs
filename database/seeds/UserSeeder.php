0f48ef4fd69303bb80d83ce29e923173fa917d05
KomelAbbas <komelabbasweb786@gmail.com>
2019-06-21 20:02:38 +0530

    password string mistake

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,7 +14,7 @@ class UserSeeder extends Seeder
         DB::table('users')->insert([
             'name' => "Bytefury" ,
             'email' => "bytefury@gmail.com" ,
-            'password' => "$2y$10$vJJ8.YPtd81zQqgfUackSeuP6UtVOu9TSAA9Vh2CxqPJgTNKXt1PK"
+            'password' => '$2y$10$vJJ8.YPtd81zQqgfUackSeuP6UtVOu9TSAA9Vh2CxqPJgTNKXt1PK'
         ]);
     }
 }
